bc09d0952e1cc99ac5aacfaf0d7c5b5e2f8f0d4a
andrs4 <andrs@byui.edu>
2022-01-25 22:40:24 -0400

    string change to variable string

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        String hello = "Hello World";
+        System.out.println(hello);
+
     }
 }
